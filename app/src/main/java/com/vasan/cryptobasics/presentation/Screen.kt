1e2200771f9442c7045b17dc62783a5d67cbe005
Vasanthakumar95 <vasantrajamanickam@gmail.com>
2022-07-04 13:37:58 +0800

    - Removed unwanted Resources - added new icon - added reminders and notes

--- app/src/main/java/com/vasan/cryptobasics/presentation/Screen.kt
+++ app/src/main/java/com/vasan/cryptobasics/presentation/Screen.kt
@@ -1,6 +1,7 @@
 package com.vasan.cryptobasics.presentation
 
 sealed class Screen(val route: String){
+    object SplashScreen: Screen("splash_screen")
     object CoinListScreen: Screen("coin_list_screen")
     object CoinDetailsScreen: Screen("coin_details_screen")
 }
